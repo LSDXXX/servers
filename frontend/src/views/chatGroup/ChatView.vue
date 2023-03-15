0c8aa0540fdd37b311d4800790f38d63551ac26b
sidaliang <sidaliang@tencent.com>
2023-03-15 09:53:21 +0800

    fixed

--- frontend/src/views/chatGroup/ChatView.vue
+++ frontend/src/views/chatGroup/ChatView.vue
@@ -8,7 +8,7 @@
       >
         <div class="message">
           <div class="from">{{ message.from }}</div>
-          <div v-html="content(message.content)"></div>
+          <div class="from" v-html="content(message.content)"></div>
         </div>
       </div>
     </div>
@@ -20,12 +20,26 @@
 </template>
 
 <script>
+import Clipboard from "clipboard";
 export default {
+  mounted() {
+    this.$nextTick(() => {
+      this.clipboard = new Clipboard(".copy-btn");
+      // 复制成功失败的提示
+      this.clipboard.on("success", () => {
+        this.$message.success("复制成功");
+      });
+      this.clipboard.on("error", () => {
+        this.$message.error("复制成功失败");
+      });
+    });
+  },
   data() {
     return {
       messages: [],
       message: "",
       socket: null,
+      clipboard: null,
     };
   },
   created() {
@@ -84,22 +98,42 @@ export default {
         breaks: true,
         typographer: true,
         highlight: function (str, lang) {
+          // 当前时间加随机数生成唯一的id标识
+          const codeIndex =
+            parseInt(Date.now()) + Math.floor(Math.random() * 10000000);
+          // 复制功能主要使用的是 clipboard.js
+          let html = `<button class="copy-btn" type="button" data-clipboard-action="copy" data-clipboard-target="#copy${codeIndex}">复制</button>`;
+          const linesLength = str.split(/\n/).length - 1;
+          // 生成行号
+          let linesNum = '<span aria-hidden="true" class="line-numbers-rows">';
+          for (let index = 0; index < linesLength; index++) {
+            linesNum = linesNum + "<span></span>";
+          }
+          linesNum += "</span>";
+
           if (lang && hljs.getLanguage(lang)) {
             try {
-              return (
-                '<pre class="hljs"><code>' +
-                hljs.highlight(lang, str, true).value +
-                "</code></pre>"
-              );
-            } catch (__) {
-              return "";
+              // highlight.js 高亮代码
+              const preCode = hljs.highlight(lang, str, true).value;
+              html = html + preCode;
+              if (linesLength) {
+                html += '<b class="name">' + lang + "</b>";
+              }
+              // 将代码包裹在 textarea 中，由于防止textarea渲染出现问题，这里将 "<" 用 "&lt;" 代替，不影响复制功能
+              return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str.replace(
+                /<\/textarea>/g,
+                "&lt;/textarea>"
+              )}</textarea>`;
+            } catch (error) {
+              console.log(error);
             }
           }
-          return (
-            '<pre class="hljs"><code>' +
-            md.utils.escapeHtml(str) +
-            "</code></pre>"
-          );
+          const preCode = md.utils.escapeHtml(str);
+          html = html + preCode;
+          return `<pre class="hljs"><code>${html}</code>${linesNum}</pre><textarea style="position: absolute;top: -9999px;left: -9999px;z-index: -9999;" id="copy${codeIndex}">${str.replace(
+            /<\/textarea>/g,
+            "&lt;/textarea>"
+          )}</textarea>`;
         },
       });
       return md.render(data);
@@ -150,5 +184,84 @@ button {
   color: #fff;
   border: none;
   cursor: pointer;
+}
+</style>
+<style lang="less">
+pre.hljs {
+  padding: 12px 2px 12px 40px !important;
+  border-radius: 5px !important;
+  position: relative;
+  font-size: 14px !important;
+  line-height: 22px !important;
+  overflow: hidden !important;
+  code {
+    display: block !important;
+    margin: 0 10px !important;
+    overflow-x: auto !important;
+    &::-webkit-scrollbar {
+      z-index: 11;
+      width: 6px;
+    }
+    &::-webkit-scrollbar:horizontal {
+      height: 6px;
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 5px;
+      width: 6px;
+      background: #666;
+    }
+    &::-webkit-scrollbar-corner,
+    &::-webkit-scrollbar-track {
+      background: #1e1e1e;
+    }
+    &::-webkit-scrollbar-track-piece {
+      background: #1e1e1e;
+      width: 6px;
+    }
+  }
+  .line-numbers-rows {
+    position: absolute;
+    pointer-events: none;
+    top: 12px;
+    bottom: 12px;
+    left: 0;
+    font-size: 100%;
+    width: 40px;
+    text-align: center;
+    letter-spacing: -1px;
+    border-right: 1px solid rgba(0, 0, 0, 0.66);
+    user-select: none;
+    counter-reset: linenumber;
+    span {
+      pointer-events: none;
+      display: block;
+      counter-increment: linenumber;
+      &:before {
+        content: counter(linenumber);
+        color: #999;
+        display: block;
+        text-align: center;
+      }
+    }
+  }
+  b.name {
+    position: absolute;
+    top: 2px;
+    right: 50px;
+    z-index: 10;
+    color: #999;
+    pointer-events: none;
+  }
+  .copy-btn {
+    position: absolute;
+    top: 2px;
+    right: 4px;
+    z-index: 10;
+    color: #333;
+    cursor: pointer;
+    background-color: #fff;
+    border: 0;
+    border-radius: 2px;
+  }
 }
 </style>
